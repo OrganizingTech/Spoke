d1f655e2afe92879905d2b24fdd639b24042395d
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-03-11 15:23:03 -0700

    Establishes an AdminTextPreview component.

---
+++ src/components/AdminTextPreview.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminTextPreview = ({text}) => {
+    return (
+        <div>
+            "I am and Admin Text Preview"
+             TEXT PREVIEW: {text}
+        </div>
+    );
+};
+
+export default AdminTextPreview;
