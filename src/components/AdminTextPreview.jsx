b1b8131249906a2efad49f808bc0f6d140d22776
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-03-13 19:17:36 -0700

    Updates AdminTextPreview to accept only one arguement, ideally a campaign prop

--- src/components/AdminTextPreview.jsx
+++ src/components/AdminTextPreview.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { applyScript } from '../lib/scripts'
+// I might have to add an ingestMethod just go get the first line of data.
 
-const AdminTextPreview = ({text}) => {
+const AdminTextPreview = ({campaignData}) => {
+    console.log('LOOK HERE FIRST! ----------------------------- ADMINTEXTPREVIEW COMPONENT');
+    console.log(campaignData)
+
+    // const value = campaignProps
+    // console.log('ALLFIELDS');
+    // console.log(allFields);
+    // console.log('TYPE');
+    // console.log(type);
+    // console.log('SCRIPT-TEXT');
+    // console.log(sampleScript);
+    // console.log('SAMPLE-TEXTER');
+    
+    // const sampleTexter = {contactId: "40", texterFirstName:'Happy'};
+    // console.log(sampleTexter);
+    
+    const value = applyScript({
+        contact: "test contact", 
+        texter: "texter",
+        script: "test script", 
+        customFields: "custom fields"
+    })
+
     return (
-        <div>
-            "I am and Admin Text Preview"
-             TEXT PREVIEW: {text}
+        <div className='admin-text-preview-container'>
+             {value}
         </div>
     );
 };
